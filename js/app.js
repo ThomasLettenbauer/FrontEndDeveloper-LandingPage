0b9a85112328c12335a2afb92b2df9e0e4bd11d6
BuildTools <unconfigured@null.spigotmc.org>
2019-11-05 07:32:25 +0100

    smooth scroll working in Firefox and Chrome, but not in Edge

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@
  * 
 */
 
+var sections = document.querySelectorAll("section");
+var navHeight; // Height of Navigation
 
 /**
  * End Global Variables
@@ -25,7 +27,16 @@
  * 
 */
 
-
+// Check if element is in Viewport
+function isInViewPort(testElement) {
+    var boundingElement = testElement.getBoundingClientRect();
+    return (
+        boundingElement.left >= 0 &&
+        boundingElement.top >= 0 &&
+        boundingElement.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        boundingElement.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
 
 /**
  * End Helper Functions
@@ -34,24 +45,70 @@
 */
 
 // build the nav
+function buildMenu() {
 
+    var navName;
+    var sectionName;
+    var navList = document.querySelector("#navbar__list");
+
+    for (var i = 0; i < sections.length; i++) {
+        navName = sections[i].getAttribute('data-nav');
+        sectionName = sections[i].getAttribute('id');
+        navList.innerHTML += '<li class="menu__link"><a href="#' + sectionName + '">' + navName + '</a></li>';
+    }
+
+    navHeight = document.querySelector('header').getBoundingClientRect().height;
+}
 
 // Add class 'active' to section when near top of viewport
+function checkActiveSection() {
+
+    for (var i = 0; i < sections.length; i++) {
+
+        if (isInViewPort(sections[i])) {
+            sections[i].classList.add('active');
+        } else {
+            sections[i].classList.remove('active');
+        }
+    }
+
+}
+
 
 
 // Scroll to anchor ID using scrollTO event
-
+function smoothScrollingTo(target) {
+    var myLinkedElement = document.querySelector(target.getAttribute('href'));
+    var scrollTop = myLinkedElement.offsetTop - navHeight;
+    var scrollLeft = myLinkedElement.offsetLeft;
+    window.scrollTo({
+        top: scrollTop,
+        left: scrollLeft,
+        behavior: 'smooth'
+    });
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
+addEventListener('load', function () {
+    buildMenu();
+})
 
 // Scroll to section on link click
+addEventListener("click", function (event) {
+    event.preventDefault();
+    myTarget = event.target;
+    smoothScrollingTo(myTarget);
+});
+
 
 // Set sections as active
-
+addEventListener('scroll', function () {
+    checkActiveSection();
+})
 
